2b3007b5748d46ed27b7a8bf33b0e0cdaaaadccb
MatheusLemeTech <matheuspazleme@gmail.com>
2025-11-05 11:59:34 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:aula6qi/screens/myhomepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aula 6',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage()
+    );
+  }
+}
+
